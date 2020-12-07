422ae9c9e1985cc89d4124b60df5bce89caf48e6
Noah Varghese <noah.varghese@websan.com>
2020-12-07 16:09:26 -0500

    got transition working, and form looking okay

--- components/grades.vue.js
+++ components/grades.vue.js
@@ -6,46 +6,53 @@ const Grades = {
             </nav>
             <h1 class="title">Grades</h1>
             <div id="container">
-                <div>
-                    <input type="search" name="search" placeholder="Search" id="search" />
-                    <button v-on:click="show = !show">Create</button>
+                <div id="toolBar">
+                    <div>
+                        <input v-if="show" type="search" name="search" placeholder="Search" id="search" />
+                    </div>
+                    <button v-on:click="show = !show"><span v-if="show">Create</span><span v-else>Cancel</span></button>
                 </div>
-                <table>
-                    <transition name="rotate-table">
-                        <div id="allGrades" v-if="show">
-                            <tr id="columnHeadings">
-                                <th class="course">
-                                    Course
-                                    <div class="sort">
-                                        <img class="sortImg" src="/assets/img/asc.png" alt="ascending" />
-                                        <img class="sortImg" src="/assets/img/desc.png" alt="descending" />
-                                    </div>
-                                </th>
-                                <th class="grade">
-                                    Grade
-                                    <div class="sort">
-                                        <img class="sortImg" src="/assets/img/asc.png" alt="ascending" />
-                                        <img class="sortImg" src="/assets/img/desc.png" alt="descending" />
-                                    </div>
-                                </th>
-                                <th></th>
-                            </tr>
-                            <tr v-for="grade in grades" v-bind:key="grade.course">
-                                <td>{{ grade.course }}</td>
-                                <td>{{ grade.grade }}</td>
-                                <td>
-                                    <div class="imgContainer">
-                                        <img src="/assets/img/edit.png" alt="edit" />
-                                        <img src="/assets/img/delete.png" alt="delete" />
-                                    </div>
-                                </td>
-                            </tr>
-                        </div>
-                        <div id="createGrade" v-else>
-                            <h4>Create New</h4>
-                        </div>
-                    </transition>
-                </table>
+                <transition name="flip" mode="out-in">
+                    <table id="allGrades" v-if="show">
+                        <tr id="columnHeadings">
+                            <th class="course">
+                                Course
+                                <div class="sort">
+                                    <img class="sortImg" src="/assets/img/asc.png" alt="ascending" />
+                                    <img class="sortImg" src="/assets/img/desc.png" alt="descending" />
+                                </div>
+                            </th>
+                            <th class="grade">
+                                Grade
+                                <div class="sort">
+                                    <img class="sortImg" src="/assets/img/asc.png" alt="ascending" />
+                                    <img class="sortImg" src="/assets/img/desc.png" alt="descending" />
+                                </div>
+                            </th>
+                            <th></th>
+                        </tr>
+                        <tr v-for="grade in grades" v-bind:key="grade.course">
+                            <td>{{ grade.course }}</td>
+                            <td>{{ grade.grade }}</td>
+                            <td>
+                                <div class="imgContainer">
+                                    <img src="/assets/img/edit.png" alt="edit" />
+                                    <img src="/assets/img/delete.png" alt="delete" />
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                    <div id="createGrade" v-else>
+                        <div>
+                            <h2>Create New</h2>
+                        </div>    
+                        <form>
+                            <input type="text" name="course" id="course" aria-label="course" placeholder="Course" />
+                            <input type="number" name="grade" id="grade" aria-label="grade" placeholder="99" />
+                            <button>Create</button>
+                        </form>
+                    </div>
+                </transition>
             </div>
         </div>
     `,
