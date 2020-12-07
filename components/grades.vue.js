d1318e30150b32e843d384322c66233dc8101db5
Noah Varghese <noah.varghese@websan.com>
2020-12-07 00:02:05 -0500

    got transition working, need animation

--- components/grades.vue.js
+++ components/grades.vue.js
@@ -8,36 +8,50 @@ const Grades = {
             <div id="container">
                 <div>
                     <input type="search" name="search" placeholder="Search" id="search" />
-                    <button>Create</button>
+                    <button v-on:click="show = !show">Create</button>
                 </div>
                 <table>
-                    <tr id="columnHeadings">
-                        <th>
-                            Course
-                            <div class="sort">
-                                <img class="sortImg" src="/assets/img/asc.png" alt="ascending" />
-                                <img class="sortImg" src="/assets/img/desc.png" alt="descending" />
-                            </div>
-                        </th>
-                        <th>
-                            Grade
-                            <div class="sort">
-                                <img class="sortImg" src="/assets/img/asc.png" alt="ascending" />
-                                <img class="sortImg" src="/assets/img/desc.png" alt="descending" />
-                            </div>
-                        </th>
-                        <th></th>
-                    </tr>
-                        <tr v-for="grade in grades" v-bind:key="grade.course">
-                            <td>{{ grade.course }}</td>
-                            <td>{{ grade.grade }}</td>
-                        </tr>
+                    <transition name="rotate-table">
+                        <div id="allGrades" v-if="show">
+                            <tr id="columnHeadings">
+                                <th class="course">
+                                    Course
+                                    <div class="sort">
+                                        <img class="sortImg" src="/assets/img/asc.png" alt="ascending" />
+                                        <img class="sortImg" src="/assets/img/desc.png" alt="descending" />
+                                    </div>
+                                </th>
+                                <th class="grade">
+                                    Grade
+                                    <div class="sort">
+                                        <img class="sortImg" src="/assets/img/asc.png" alt="ascending" />
+                                        <img class="sortImg" src="/assets/img/desc.png" alt="descending" />
+                                    </div>
+                                </th>
+                                <th></th>
+                            </tr>
+                            <tr v-for="grade in grades" v-bind:key="grade.course">
+                                <td>{{ grade.course }}</td>
+                                <td>{{ grade.grade }}</td>
+                                <td>
+                                    <div class="imgContainer">
+                                        <img src="/assets/img/edit.png" alt="edit" />
+                                        <img src="/assets/img/delete.png" alt="delete" />
+                                    </div>
+                                </td>
+                            </tr>
+                        </div>
+                        <div id="createGrade" v-else>
+                            <h4>Create New</h4>
+                        </div>
+                    </transition>
                 </table>
             </div>
         </div>
     `,
     data() {
         return {
+            show: true,
             grades: [
                 {
                     course: "Emerging Web Tech",
