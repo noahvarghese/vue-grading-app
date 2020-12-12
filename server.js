9b0184eee6cbd6d5654879b1f7c45ff591448f5a
noahvarghese <varghese.noah@gmail.com>
2020-12-12 14:30:38 -0500

    added simple server

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const path = require("path");
 (async () => {
     const app = express();
 
-    app.get("/", res.sendFile(path.join(__dirname, "index.html")));
+    app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+    // static files (css/js/etc)
+    app.get(/^(.+)$/, (req, res) => {
+        console.log("static file request: " + req.params[0]);
+        res.sendFile(__dirname + req.params[0]);
+    });
+
     const port = 4000;
     app.listen(port, () => {
         console.log(`Server started on port: ${port}`);
